92ec11ad924f6c0ad0e9c83462bc01021c227ce1
grzegorekb <bartlomiejgrzegorek@gmail.com>
2021-06-19 15:38:32 +0200

    switch to 'exec' api with commands

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,34 +1,59 @@
 import { getBackendSrv } from '@grafana/runtime';
 import { DatapointSettings, ProjectSettings, SubsystemSettings } from 'types';
-import { API_ROOT_PAGE } from './consts';
+import { API_EXEC_PATH } from './consts';
 
+interface Command {
+  resource: string;
+  action: string;
+  params?: any;
+  payload?: any;
+}
+
+const exec = (cmd: Command) => getBackendSrv().post(API_EXEC_PATH, cmd);
+
+// projects
 export const upsertProject = (project: ProjectSettings) =>
-  getBackendSrv().put(`${API_ROOT_PAGE}/resources/projects/${project.name}`, project);
+  exec({
+    action: 'update',
+    resource: 'project',
+    payload: project,
+  });
+
 export const getProject = (name: string): Promise<ProjectSettings> =>
-  getBackendSrv().get(`${API_ROOT_PAGE}/resources/projects/${name}`);
-export const getProjects = (): Promise<ProjectSettings[]> => getBackendSrv().get(`${API_ROOT_PAGE}/resources/projects`);
-export const deleteProject = (name: string) => getBackendSrv().delete(`${API_ROOT_PAGE}/resources/projects/${name}`);
+  exec({
+    action: 'get',
+    resource: 'project',
+    params: {
+      project: name,
+    },
+  });
+
+export const getProjects = (): Promise<ProjectSettings[]> => exec({ action: 'list', resource: 'project' });
+
+export const deleteProject = (name: string) =>
+  exec({ action: 'delete', resource: 'project', params: { project: name } });
 
 // subsystems
 export const getSubsystems = (projectName: string): Promise<SubsystemSettings[]> =>
-  getBackendSrv().get(`${API_ROOT_PAGE}/resources/projects/${projectName}/subsystems`);
+  exec({ action: 'list', resource: 'subsystem', params: { project: projectName } });
 
-export const addSubsystem = (projectName: string, subsystem: SubsystemSettings) => {
-  return getBackendSrv().put(`${API_ROOT_PAGE}/resources/projects/${projectName}/subsystems`, subsystem);
-};
+export const addSubsystem = (projectName: string, subsystem: SubsystemSettings) =>
+  exec({ action: 'update', resource: 'subsystem', payload: subsystem, params: { project: projectName } });
 
-export const deleteSubsystem = (name: string): Promise<void> => {
-  // toDo
-  // return loadSubsystems(projectName);
-  return Promise.resolve();
-};
+export const deleteSubsystem = (name: string): Promise<void> =>
+  exec({ action: 'delete', resource: 'subsystem', params: { subsystem: name } });
 
 // datapoints
 export const getDatapoints = (projectName: string, subsystemName: string): Promise<DatapointSettings[]> =>
-  getBackendSrv().get(`${API_ROOT_PAGE}/resources/projects/${projectName}/subsystems/${subsystemName}/datapoints`);
+  exec({ action: 'list', resource: 'datapoint', params: { project: projectName, subsystem: subsystemName } });
 
 export const addDatapoint = (projectName: string, subsystemName: string, datapoint: DatapointSettings) =>
-  getBackendSrv().put(
-    `${API_ROOT_PAGE}/resources/projects/${projectName}/subsystems/${subsystemName}/datapoints`,
-    datapoint
-  );
+  exec({
+    action: 'update',
+    resource: 'datapoint',
+    payload: datapoint,
+    params: { project: projectName, subsystem: subsystemName },
+  });
+
+export const deleteDatapoint = (name: string): Promise<void> =>
+  exec({ action: 'delete', resource: 'datapoint', params: { datapoint: name } });
