af78f7539e31278419bc2ef8109333fbf370e4bf
grzegorekb <bartlomiejgrzegorek@gmail.com>
2021-06-19 15:42:26 +0200

    fix resources/exec endpoint

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,4 +2,4 @@ export const APP_TITLE = 'Sensetif App Plugin';
 export const APP_SUBTITLE = 'Visualizing Your Stuff';
 
 export const API_ROOT_PAGE = '/api/plugins/sensetif-datasource';
-export const API_EXEC_PATH = `${API_ROOT_PAGE}/exec`;
+export const API_EXEC_PATH = `${API_ROOT_PAGE}/resources/exec`;
