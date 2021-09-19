06ea417dae9f09d01f9acaa67fd411325987a34f
niclas <niclas@hedhman.org>
2021-09-19 19:52:43 +0200

    Need help from grzegorekb to do the remote call instead of <form> as well as fixing up the layout to be better looking.

--- src/components/PlansList.tsx
+++ src/components/PlansList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HorizontalGroup, Container, VerticalGroup, LoadingPlaceholder, Button } from '@grafana/ui';
+import { Button, Container, HorizontalGroup, LoadingPlaceholder, VerticalGroup } from '@grafana/ui';
 import { PollIntervals, TimeToLivePeriods } from '../utils/consts';
 import { PlanSettings } from '../types';
 
@@ -20,51 +20,55 @@ export const PlansList = (props: Props) => {
   return (
     <>
       <section className="card-section card-list-layout-list">
-        <form action="/create-checkout-session" method="POST">
-          <ol className="card-list">
-            {props.plans.products.map((product, index) => {
-              return (
-                <li className="card-item-wrapper" key={product.id} aria-label="check-card">
-                  <div className="card-item" style={{ cursor: 'pointer' }}>
-                    <HorizontalGroup justify="space-between">
-                      <HorizontalGroup justify="flex-start">
-                        <Container margin="xs">
-                          <i className={'fa fa-project-diagram'} />
-                        </Container>
-                        <VerticalGroup>
-                          <div className="card-item-name">{product.name}</div>
-                          <div className="card-item-type">{product.metadata.slogan}</div>
-                          <br />
-                          <div>{product.description}</div>
-                          <br />
-                          <div>Max Datapoints: {product.metadata.maxdatapoints}</div>
-                          <div>Max Storage Period: {getProperty(TimeToLivePeriods, product.metadata.maxstorage)}</div>
-                          <div>Min Poll Interval: {getProperty(PollIntervals, product.metadata.minpollinterval)}</div>
-                        </VerticalGroup>
-                        <VerticalGroup>
-                          {props.plans.prices
-                            .filter((price) => price.product.id === product.id)
-                            .reverse()
-                            .map((price) => {
-                              return (
-                                <div key={price.id}>
-                                  {price.recurring ? price.recurring.interval : '10 years'} :
+        <ol className="card-list">
+          {props.plans.products.map((product, index) => {
+            return (
+              <li className="card-item-wrapper" key={product.id} aria-label="check-card">
+                <div className="card-item" style={{ cursor: 'pointer' }}>
+                  <HorizontalGroup justify="space-between">
+                    <HorizontalGroup justify="flex-start">
+                      <Container margin="xs">
+                        <i className={'fa fa-project-diagram'} />
+                      </Container>
+                      <VerticalGroup>
+                        <div className="card-item-name">{product.name}</div>
+                        <div className="card-item-type">{product.metadata.slogan}</div>
+                        <br />
+                        <div>{product.description}</div>
+                        <br />
+                        <div>Max Datapoints: {product.metadata.maxdatapoints}</div>
+                        <div>Max Storage Period: {getProperty(TimeToLivePeriods, product.metadata.maxstorage)}</div>
+                        <div>Min Poll Interval: {getProperty(PollIntervals, product.metadata.minpollinterval)}</div>
+                      </VerticalGroup>
+                      <VerticalGroup>
+                        {props.plans.prices
+                          .filter((price) => price.product.id === product.id)
+                          .reverse()
+                          .map((price) => {
+                            return (
+                              //TODO: We must use Grafana's Forms, so that session data is transferred in the request
+                              //TODO: This layout is super messy, and hoping grzegorekb will fix it.
+                              <form
+                                key={price.id}
+                                action="http://localhost:3000/api/plugins/sensetif-datasource/resources/_plans/checkout"
+                                method="POST"
+                              >
+                                <input type="hidden" name="price" value={price.id} />
+                                <Button className="" type="submit">
+                                  {price.recurring ? price.recurring.interval + 'ly' : '10 years'} : &nbsp;
                                   {price.currency.toUpperCase()} {price.unit_amount / 100}
-                                </div>
-                              );
-                            })}
-                        </VerticalGroup>
-                      </HorizontalGroup>
+                                </Button>
+                              </form>
+                            );
+                          })}
+                      </VerticalGroup>
                     </HorizontalGroup>
-                    <Button className="button-red" type="submit">
-                      Subscribe!
-                    </Button>
-                  </div>
-                </li>
-              );
-            })}
-          </ol>
-        </form>
+                  </HorizontalGroup>
+                </div>
+              </li>
+            );
+          })}
+        </ol>
       </section>
     </>
   );
