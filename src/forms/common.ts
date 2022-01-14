68814f63a31d17c19da2dacc97b4e4c26dcb3776
niclas <niclas@hedhman.org>
2022-01-14 13:42:44 +0100

    Lint fix.

--- src/forms/common.ts
+++ src/forms/common.ts
@@ -1,3 +1,3 @@
-export const   PROJECT_PATTERN_NAME = /^[a-zA-Z][a-zA-Z0-9_.\-]*$/;
+export const PROJECT_PATTERN_NAME = /^[a-zA-Z][a-zA-Z0-9_.\-]*$/;
 export const SUBSYSTEM_PATTERN_NAME = /^[a-zA-Z][a-zA-Z0-9_.\-]*$/;
 export const DATAPOINT_PATTERN_NAME = /^[a-zA-Z][a-zA-Z0-9_.\-$\[\]]*$/;
