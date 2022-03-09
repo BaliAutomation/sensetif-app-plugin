432d3b8fd4a8d0b9242cb25914abf7caeb0beb47
niclas <niclas@hedhman.org>
2022-03-09 13:51:47 +0100

    Missed one name as a string. I hate when type safety is not present.

--- src/forms/datapoint/Ttnv3DatasourceForm.tsx
+++ src/forms/datapoint/Ttnv3DatasourceForm.tsx
@@ -63,7 +63,7 @@ export const Ttnv3DatasourceForm: FC<Props> = ({ register, formState: { errors }
       </Field>
 
       <Field label="Port" invalid={!!errors?.port} error={errors?.port && errors?.port.message}>
-        <Input {...register('point', {})} placeholder="Value of f_Port, leave -1 for all ports" />
+        <Input {...register('port', {})} placeholder="Value of f_Port, leave -1 for all ports" />
       </Field>
 
       <Field
