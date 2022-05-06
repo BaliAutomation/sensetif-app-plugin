2a5870ae6a2055de7f442d27dfcea331ac08c296
niclas <niclas@hedhman.org>
2022-05-06 10:51:54 +0200

    Messages don't always reach the App plugin.

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -36,6 +36,7 @@ const Notifications = ({ addr }: { addr: LiveChannelAddress }) => {
   useEffect(() => {
     const stream = getGrafanaLiveSrv().getStream<Notification>(addr);
     stream.subscribe((event) => {
+      console.log('event:', event);
       if (event.type === LiveChannelEventType.Message) {
         // use functional updated to not pass `data` as a dependency
         // otherwise `useEffect` will be triggered too often
