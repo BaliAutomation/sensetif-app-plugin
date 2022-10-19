91adfb8580f5a3d359bd41abe1c42677e2839cd2
niclas <niclas@hedhman.org>
2022-10-19 16:48:26 +0200

    npx is no longer on PATH, and don't know why.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ rm -rf dist 2>/dev/null
 yarn build --preserveConsole|| exit 1
 
 export GRAFANA_API_KEY=eyJrIjoiMzkwNTNkZTgxZTA4ODBjY2Q2YTIwNzg1NzBjZDAyOTNjOGNkZDU3OCIsIm4iOiJQdWJsaXNoIEtleSIsImlkIjo0OTA0MDZ9
-npx @grafana/toolkit plugin:sign --rootUrls "https://sensetif.net/,http://localhost:3000/"
+$HOME/node_modules/npx/node_modules/.bin/npx @grafana/toolkit plugin:sign --rootUrls "https://sensetif.net/,http://localhost:3000/"
 
 mkdir $BUILD_DIR/sensetif-app 2>/dev/null
 cp -r dist/* $BUILD_DIR/sensetif-app
