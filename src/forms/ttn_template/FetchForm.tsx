02b71115d5654c686ba5dc9afe1060ae0caf7f75
grzegorekb <bartlomiejgrzegorek@gmail.com>
2022-11-17 21:05:08 +0100

    tmp

--- src/forms/ttn_template/FetchForm.tsx
+++ src/forms/ttn_template/FetchForm.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { UseFormReturn } from 'react-hook-form';
 import { ThingsNetworkApplicationSettings } from '../../types';
-import { Button, Field, HorizontalGroup, Input, InputControl, RadioButtonGroup } from '@grafana/ui';
+import { Button, Field, FormAPI, HorizontalGroup, Input, InputControl, RadioButtonGroup, } from '@grafana/ui';
 
 export type formValues = {
   zone: string;
@@ -9,80 +8,74 @@ export type formValues = {
   token: string;
 };
 
-export interface TtnProps extends UseFormReturn<ThingsNetworkApplicationSettings> {
+export interface TtnProps extends FormAPI<ThingsNetworkApplicationSettings> {
   ttn?: ThingsNetworkApplicationSettings;
   onSubmit: (formValues: formValues) => void;
 }
 
-export const TtnResource = ({ onSubmit, control, watch, formState: { errors } }: TtnProps) => {
+export const TtnResource = ({ register, control, watch, errors }: TtnProps) => {
   const zone = watch('zone');
   const application = watch('application');
   const authorizationKey = watch('authorizationKey');
 
   return (
     <>
-      <HorizontalGroup>
-        <HorizontalGroup>
-          <Field label="Zone">
-            <InputControl
-              render={({ field: { ref, ...field } }) => (
-                <RadioButtonGroup
-                  {...field}
-                  options={[
-                    { label: 'eu1', value: 'eu1' },
-                    { label: 'nam1', value: 'nam1' },
-                    { label: 'au1', value: 'au1' },
-                  ]}
-                />
-              )}
-              rules={{
-                required: 'Zone is required',
-              }}
-              control={control}
-              defaultValue={zone ?? 'eu1'}
-              name="zone"
-            />
-          </Field>
-        </HorizontalGroup>
+      <HorizontalGroup align={'flex-start'}>
+        <Field label="Zone" >
+          <InputControl
+            render={({ field: { ref, ...field } }) => (
+              <RadioButtonGroup
+                {...field}
+                options={[
+                  { label: 'eu1', value: 'eu1' },
+                  { label: 'nam1', value: 'nam1' },
+                  { label: 'au1', value: 'au1' },
+                ]}
+              />
+            )}
+            rules={{
+              required: 'Zone is required',
+            }}
+            control={control}
+            defaultValue={zone ?? 'eu1'}
+            name="zone"
+          />
+        </Field>
 
         <Field
           label="Authorization Key"
-          invalid={!!errors?.authorizationKey}
+          invalid={!!errors.authorizationKey}
           error={errors?.authorizationKey && errors?.authorizationKey.message}
         >
-          <InputControl
-            render={({ field: { ref, ...field } }) => <Input {...field} placeholder="Authorization Key" />}
-            control={control}
-            rules={{ required: { value: true, message: 'Required.' } }}
-            name="authorizationKey"
+
+          <Input
+            {...register('authorizationKey', {
+              required: 'Authorization key is required'
+            })}
             defaultValue={authorizationKey ?? ''}
+            placeholder={'Authorization Key'}
           />
         </Field>
+
         <Field
           label="Application"
-          invalid={!!errors?.application}
-          error={errors?.application && errors?.application.message}
+          invalid={!!errors.application}
+          error={errors.application && errors.application.message}
         >
-          <InputControl
-            render={({ field: { ref, ...field } }) => <Input {...field} placeholder="Application" />}
-            control={control}
-            rules={{ required: { value: true, message: 'Required.' } }}
-            name="application"
+          <Input
+            {...register('application', { required: 'Application is required' })}
+            placeholder={'Application'}
             defaultValue={application ?? ''}
           />
         </Field>
 
         {/* Fetch Devices */}
         <Button
-          type="button"
-          variant={'secondary'}
-          onClick={() => {
-            onSubmit({
-              app: application,
-              token: authorizationKey,
-              zone: zone,
-            });
-          }}
+          name="submit"
+          style={{ marginTop: '18px' }}
+          type="submit"
+          variant={'primary'}
+          icon={'download-alt'}
         >
           {'Fetch devices'}
         </Button>
