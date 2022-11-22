ed417c41bfaaa6601af4c8151d57f106149633ba
niclas <niclas@hedhman.org>
2022-11-22 18:21:09 +0100

    New version

--- build.sh
+++ build.sh
@@ -11,5 +11,7 @@ yarn build --preserveConsole|| exit 1
 export GRAFANA_API_KEY=eyJrIjoiMzkwNTNkZTgxZTA4ODBjY2Q2YTIwNzg1NzBjZDAyOTNjOGNkZDU3OCIsIm4iOiJQdWJsaXNoIEtleSIsImlkIjo0OTA0MDZ9
 npx @grafana/toolkit plugin:sign --rootUrls "https://sensetif.net/,http://localhost:3000/,https://staging.sensetif.net/"
 
+sudo service grafana-server stop
 mkdir $BUILD_DIR/sensetif-app 2>/dev/null
 cp -r dist/* $BUILD_DIR/sensetif-app
+sudo service grafana-server start
