6cbbdb2bc20587f8a8b91c285757810580922c67
niclas <niclas@hedhman.org>
2023-02-18 15:43:40 +0100

    Cleaning up syntax to be consistent.

--- src/sensetif-interactivetable-panel/components/InteractiveTablePanel.tsx
+++ src/sensetif-interactivetable-panel/components/InteractiveTablePanel.tsx
@@ -8,7 +8,8 @@ import { InteractiveTable, UpdateValue } from './InteractiveTable';
 import { ResourceSettings, TsPair } from 'types';
 import { updateTimeseriesValues } from 'utils/api';
 
-interface Props extends PanelProps<SimpleOptions> { }
+interface Props extends PanelProps<SimpleOptions> {
+}
 
 const getStyles = () => {
   return {
@@ -27,10 +28,10 @@ const getStyles = () => {
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   if (!isValidData(data)) {
-    console.warn("panel is only comaptible with sensetif datasource")
+    console.warn('panel is only comaptible with sensetif datasource');
   }
 
-  const [showConfirmation, setShowConfirmation] = React.useState<boolean>()
+  const [showConfirmation, setShowConfirmation] = React.useState<boolean>();
 
   const styles = useStyles2(getStyles);
 
@@ -47,7 +48,7 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
     tsPairs: TsPair[]
   }
 
-  const [state, setState] = React.useState<updates>({})
+  const [state, setState] = React.useState<updates>({});
 
   const makeRequests = (): request[] => {
     return Object.keys(state).map((refId) => {
@@ -55,60 +56,62 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
       const tsPairs = Object.entries(state[refId]).map(([timestamp, value]) => ({
         ts: timestamp,
         value: value,
-      }))
+      }));
 
       return {
         project: query.project,
         subsystem: query.subsystem,
         datapoint: query.datapoint,
         tsPairs: tsPairs
-      }
-    })
-  }
+      };
+    });
+  };
 
   const onUpdate = (value: UpdateValue) => {
     setState(state => {
-      let copy = { ...state }
+      let copy = { ...state };
       if (!copy[value.refId]) {
-        copy[value.refId] = {}
+        copy[value.refId] = {};
       }
-      copy[value.refId][dateTimeFormatISO(value.time)] = value.value
-      return copy
-    })
-  }
+      copy[value.refId][dateTimeFormatISO(value.time)] = value.value;
+      return copy;
+    });
+  };
 
   if (showConfirmation) {
     return <ConfirmModal
-      title={"Update values confirmation"}
+      title={'Update values confirmation'}
       isOpen={showConfirmation}
-      confirmText={"Confirm"}
+      confirmText={'Confirm'}
       onConfirm={() => {
         let promises: Array<Promise<ResourceSettings>> = [];
         makeRequests().forEach(req => {
           let promise = updateTimeseriesValues(req.project, req.subsystem, req.datapoint, req.tsPairs);
-          promises.push( promise );
+          promises.push(promise);
         });
         Promise.all(promises)
           .catch(reason => {
             // TODO: How to show the Grafana notification thingy?
           })
-          .finally(() => setShowConfirmation(false) )
+          .finally(() => setShowConfirmation(false));
       }}
       onDismiss={() => setShowConfirmation(false)}
       body={<>
         <pre>{JSON.stringify(makeRequests(), null, ' ')}</pre>
       </>}
-    />
+    />;
   }
 
   return (
     <div
       className={css`
-          width: ${width}px;
-          height: ${height}px;
-        `}
+        width: ${width}px;
+        height: ${height}px;
+      `}
     >
-      <Button onClick={() => { setShowConfirmation(true) }}>Confirm Update</Button>
+      <Button onClick={() => {
+        setShowConfirmation(true);
+      }}>Confirm Update</Button>
       <div className={cx(styles.wrapper)}>
         <InteractiveTable
           frames={data.series}
@@ -126,21 +129,21 @@ type sensetifQuery = DataQuery & {
 }
 
 const getQueryByRefId = (refId: string, queries: DataQuery[]): sensetifQuery => {
-  let query = queries.find(q => q.refId === refId)
-  return query as sensetifQuery
-}
+  let query = queries.find(q => q.refId === refId);
+  return query as sensetifQuery;
+};
 
 const isValidData = (data: PanelData): boolean => {
-  const targets = data.request?.targets
+  const targets = data.request?.targets;
   if (!targets) {
-    return false
+    return false;
   }
 
   for (let target of targets) {
-    if (target.datasource?.type !== "sensetif-datasource") {
-      return false
+    if (target.datasource?.type !== 'sensetif-datasource') {
+      return false;
     }
   }
 
-  return true
-}
+  return true;
+};
