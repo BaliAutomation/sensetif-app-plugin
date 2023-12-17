28cdc9155011bd69cae6587323778d822cf0f757
grzegorekb <bartlomiejgrzegorek@gmail.com>
2023-12-17 20:19:59 +0100

    extend script backend model

--- pkg/model/script.go
+++ pkg/model/script.go
@@ -1,7 +1,14 @@
 package model
 
+type ScriptParam struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
 type Script struct {
-	Name     string `json:"name"`
-	Code     string `json:"code"`
-	Language string `json:"language"`
+	Name        string        `json:"name"`
+	Code        string        `json:"code"`
+	Language    string        `json:"language"`
+	Description string        `json:"description"`
+	Scope       string        `json:"scope"`
+	Params      []ScriptParam `json:"params"`
 }
